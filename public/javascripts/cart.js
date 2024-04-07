7d1f7d6558593a40c0776f7b2768d4e7d9be27e5
danudeth2004 <oat25482547@gmail.com>
2024-04-07 16:12:27 +0700

    fix error button popup

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -14,12 +14,8 @@ function linktoendTH(){
 function activePopup(){
     const showPopup = document.querySelector('.submit-button');
     const popupContainer = document.querySelector('.popup-container');
-    const finishBtn = document.querySelector('.finish-popup-button');
     showPopup.onclick = () => {
         popupContainer.classList.add('active');
-    }
-    finishBtn.onclick = () => {
-        popupContainer.classList.remove('active');      
     }
 }
 
