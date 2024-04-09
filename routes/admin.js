53aa613f46920502ddf70bf3c300e113904a3669
danudeth2004 <oat25482547@gmail.com>
2024-04-10 02:25:34 +0700

    admin payment and serve success

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,7 @@ router.get('/', function (req, res, next) {
     const price_query  = "SELECT total_price,order_id,status FROM payment;";
     const detail_query = "SELECT Detail_id,noodles_nameEN,noodles_nameTH,meat_nameEN,meat_nameTH,topping_nameEN,topping_nameTH,veg_nameEN,veg_nameTH FROM order_detail;";
     const submit_query = "SELECT order_id,Detail_id,customer_id,serve FROM submit_order;";
+    const minNonserve = "SELECT MIN(order_id) AS order_min FROM submit_order WHERE serve=0;";
     
     database.query(time_query, (err, time_result) => {
         if (err) {
@@ -36,19 +37,46 @@ router.get('/', function (req, res, next) {
                         res.status(500).send("Error executing query");
                         return;
                     }
+                    database.query(minNonserve, (err, min_result) => {
+                        if (err) {
+                            console.error("Error executing query:", err);
+                            res.status(500).send("Error executing query");
+                            return;
+                        }
 
-                    res.render("admin", 
-                    {title: "Admin",
-                    time: time_result,
-                    price: price_result,
-                    detail: detail_result,
-                    submit: submit_result
+                        res.render("admin", 
+                        {title: "Admin",
+                        time: time_result,
+                        price: price_result,
+                        detail: detail_result,
+                        submit: submit_result,
+                        min: min_result
+                        });
                     });
                 });
             });
         });
     });
 });
+
+router.post('/', function (req, res, next) {
+    var indexId = req.body.indexId;
+    const update_payment = "UPDATE payment SET status=1 WHERE payment_id=?";
+    database.query(update_payment, [indexId], (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        else console.log("Update Status Payment Successfully.");
+    });
+    const update_serve = "UPDATE submit_order SET serve=1 WHERE order_id=?";
+    database.query(update_serve, [indexId], (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+        else console.log("Update Serve Already Submit Successfully.");
+    });
+    res.redirect('admin');
+}); 
 
 router.get('/ingredient', function (req, res, next) {
     res.render('admin/ingredient', { title: 'Admin' });
